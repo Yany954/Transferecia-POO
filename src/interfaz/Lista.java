28392bec6b9a0c78c51f759b5afac5c9fc709e95
yanyg <yanyg@DESKTOP-E0JBJ78>
2022-05-16 21:25:46 -0500

    Version 1

---
+++ src/interfaz/Lista.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interfaz;
+
+/**
+ *
+ * @author yanyg
+ */
+public class Lista implements java.io.Serializable {
+    private int[] x;
+    private int n;
+}
